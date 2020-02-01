49bf1c91b330239b16a749ea0c858a888a55eacd
kwangyong.park <pky1030@linecorp.com>
2020-02-01 14:50:46 +0900

    Init

---
+++ src/main/kotlin/com/pky/easycms/EasycmsApplication.kt
@@ -0,0 +1,11 @@
+package com.pky.easycms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasycmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasycmsApplication>(*args)
+}
---
+++ src/test/kotlin/com/pky/easycms/EasycmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pky.easycms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasycmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
