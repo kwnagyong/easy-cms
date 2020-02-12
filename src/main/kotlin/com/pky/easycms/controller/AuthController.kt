ed65ff797be4833a9ccc2b67eb23cc2cd585dddf
kwangyong.park <pky1030@linecorp.com>
2020-02-12 19:35:12 +0900

    Add POST /auth/sign-in specification

--- src/main/kotlin/com/pky/easycms/controller/AuthController.kt
+++ src/main/kotlin/com/pky/easycms/controller/AuthController.kt
@@ -1,8 +1,6 @@
 package com.pky.easycms.controller
 
-import com.pky.easycms.controller.model.auth.AddOrganizationRequest
-import com.pky.easycms.controller.model.auth.SignUpRequest
-import com.pky.easycms.controller.model.auth.SignUpResponse
+import com.pky.easycms.controller.model.auth.*
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +12,11 @@ class AuthController {
 
     @PostMapping("/sign-up")
     fun signUp(@RequestBody request: SignUpRequest): SignUpResponse? {
+        return null;
+    }
+
+    @PostMapping("/sign-in")
+    fun signIn(@RequestBody request: SignInRequest): SignInResponse? {
         return null;
     }
 
